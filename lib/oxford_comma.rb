3cb6b3d94d455e7fac520762ff909b766c6fee91
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-09-02 16:58:19 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+	if array.size == 1
+		return array[0]
+	end
+	if array.size == 2
+		return "#{array[0]} and #{array[1]}"
+	end
+	output = ""
+	for i in 0..array.size-1 do
+		if i == array.size-1
+			output << "and #{array[i]}"
+		else 
+			output += "#{array[i]}, "
+		end
+	end
+	output
 end

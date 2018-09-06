663f0250ec08e5ee762a728684ea2143a6cce4f0
Iman Jawad <imanj12@gmail.com>
2018-09-06 16:14:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  string = ""
+  if array.length == 1
+    string = array.join
+  elsif array.length == 2
+    string = array.join(" and ")
+  else
+    array.each_with_index do |ele, i|
+      if i == array.length - 1
+        string += "and #{ele}"
+      else
+        string += "#{ele}, "
+      end
+    end
+  end
+  string
 end
